fcb761ee5efe3697b0ea494b0dd494774f1c4963
JISUN HAN <my__memoria@naver.com>
2022-11-18 21:48:49 +0900

    feat: problem2 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const regex = /(.)\1+/;
+
+  if (!regex.test(cryptogram)) {
+    return cryptogram;
+  }
+
+  const newCryptogram = cryptogram.replace(regex, "");
+  return problem2(newCryptogram);
 }
 
 module.exports = problem2;
